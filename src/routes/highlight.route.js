183cae2898b900b6e5de96e8c7a48db1a1fd832a
Ankit-Unlox <ankit.gupta@unloxacademy.com>
2026-07-08 18:24:21 +0530

    highlight validated

--- src/routes/highlight.route.js
+++ src/routes/highlight.route.js
@@ -5,8 +5,9 @@ const router = require('express').Router();
 const { highlightController }         = require('../modules/highlight/highlight.container');
 const { verifyToken } = require('../middlewares/auth.middleware');
 const { validateRequest }               = require('../middlewares/validator.middleware');
+const { paginationQuerySchema } = require('../modules/highlight/highlight.validator');
 
 
-router.get('/',                        verifyToken,                                   highlightController.getSpotligth)
+router.get('/',     verifyToken,     validateRequest({query: paginationQuerySchema}),     highlightController.getSpotligth)
 
 module.exports = router

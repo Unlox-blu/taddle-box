40622ed00f1ed7c0a6bbdb68db2fda3d3c84b197
Ankit-Unlox <ankit.gupta@unloxacademy.com>
2026-07-09 12:32:00 +0530

    streak validated

--- src/routes/streak.route.js
+++ src/routes/streak.route.js
@@ -5,10 +5,11 @@ const router = require('express').Router();
 const { streakController }         = require('../modules/streak/streak.container');
 const { verifyToken, optionalAuth } = require('../middlewares/auth.middleware');
 const { validateRequest }               = require('../middlewares/validator.middleware');
+const { paginationQuerySchema } = require('../modules/streak/streak.validator');
 
 
-router.post('/',             verifyToken,                       streakController.createOrUpdate)
-router.get('/',              verifyToken,                       streakController.getCurrentStreak)
-router.get('/history',       verifyToken,                       streakController.getStreakHistory)
+router.post('/',        verifyToken,                                                        streakController.createOrUpdate)
+router.get('/',         verifyToken,                                                        streakController.getCurrentStreak)
+router.get('/history',  verifyToken,    validateRequest({query: paginationQuerySchema}),    streakController.getStreakHistory)
 
 module.exports = router

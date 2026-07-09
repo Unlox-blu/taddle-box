e92adb8dd1ed31e5867dfd3e31e8880f5a9b4931
Ankit-Unlox <ankit.gupta@unloxacademy.com>
2026-07-09 12:13:29 +0530

    search validated

--- src/routes/search.route.js
+++ src/routes/search.route.js
@@ -4,7 +4,9 @@
 const router = require('express').Router();
 const { searchController } = require('../modules/search/search.container');
 const { optionalAuth }    = require('../middlewares/auth.middleware');
+const { validateRequest } = require('../middlewares/validator.middleware');
+const { searchQuerySchema } = require('../modules/search/search.validator');
 
-router.get('/', optionalAuth,    searchController.search);
+router.get('/', optionalAuth,   validateRequest({query: searchQuerySchema}),   searchController.search);
 
 module.exports = router;

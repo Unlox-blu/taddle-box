b43bcb13eaf768b358a979f7a9d700bd5313c486
Ankit-Unlox <ankit.gupta@unloxacademy.com>
2026-07-09 12:02:50 +0530

    save validated

---
+++ src/modules/save/save.validator.js
@@ -0,0 +1,24 @@
+'use strict';
+
+const { z } = require('zod');
+
+
+const paginationQuerySchema = z.object({
+  page: z.coerce
+    .number({ invalid_type_error: "Page must be a number" })
+    .int()
+    .positive()
+    .default(1).optional(),
+
+  limit: z.coerce
+    .number({ invalid_type_error: "Limit must be a number" })
+    .int()
+    .positive()
+    .max(100, "Maximum limit allowed is 100")
+    .default(10).optional(),
+}).strict();
+
+
+module.exports = {
+  paginationQuerySchema,
+};
--- src/routes/save.route.js
+++ src/routes/save.route.js
@@ -4,7 +4,9 @@
 const router = require('express').Router();
 const { saveController } = require('../modules/save/save.container');
 const { verifyToken }      = require('../middlewares/auth.middleware');
+const { validateRequest } = require('../middlewares/validator.middleware');
+const { paginationQuerySchema } = require('../modules/save/save.validator');
 
-router.get('/',                  verifyToken,                                   saveController.getSave);
+router.get('/',                  verifyToken,      validateRequest({query: paginationQuerySchema}),         saveController.getSave);
 
 module.exports = router;

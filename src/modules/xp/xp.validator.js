cc72d24a0d6a17af21f9a57404c9d854853de54f
Ankit-Unlox <ankit.gupta@unloxacademy.com>
2026-07-09 12:53:48 +0530

    xp validated

--- src/modules/xp/xp.validator.js
+++ src/modules/xp/xp.validator.js
@@ -9,4 +9,21 @@ const creditOrdebitXPSchema = z.object({
   sourceType: z.string().min(1, { message: "Source type cannot be empty" }).max(25, { message: "Source type max 50 chars" })
 }).strict();
 
-module.exports = { creditOrdebitXPSchema };
+
+const paginationQuerySchema = z.object({
+  page: z.coerce
+    .number({ invalid_type_error: "Page must be a number" })
+    .int()
+    .positive()
+    .default(1).optional(),
+
+  limit: z.coerce
+    .number({ invalid_type_error: "Limit must be a number" })
+    .int()
+    .positive()
+    .max(100, "Maximum limit allowed is 100")
+    .default(10).optional(),
+}).strict();
+
+
+module.exports = { paginationQuerySchema, creditOrdebitXPSchema };
--- src/routes/xp.route.js
+++ src/routes/xp.route.js
@@ -5,12 +5,12 @@ const router = require('express').Router();
 const { xpController } = require('../modules/xp/xp.container');
 const { verifyToken }      = require('../middlewares/auth.middleware');
 const { validateRequest }         = require('../middlewares/validator.middleware');
-const { creditOrdebitXPSchema } = require('../modules/xp/xp.validator');
+const { creditOrdebitXPSchema, paginationQuerySchema } = require('../modules/xp/xp.validator');
 
-router.post('/',                 verifyToken,                                   xpController.createXPwallet);
-router.get('/',                  verifyToken,                                   xpController.getXP);
-router.get('/transactions',      verifyToken,                                   xpController.getTransactions);
-router.post('/credit',           verifyToken, validateRequest({ body: creditOrdebitXPSchema }),  xpController.creditXP);
-router.post('/debit',            verifyToken, validateRequest({ body: creditOrdebitXPSchema }),  xpController.debitXP);
+router.post('/',                 verifyToken,                                                       xpController.createXPwallet);
+router.get('/',                  verifyToken,                                                       xpController.getXP);
+router.get('/transactions',      verifyToken, validateRequest({ params: paginationQuerySchema }),   xpController.getTransactions);
+router.post('/credit',           verifyToken, validateRequest({ body: creditOrdebitXPSchema }),     xpController.creditXP);
+router.post('/debit',            verifyToken, validateRequest({ body: creditOrdebitXPSchema }),     xpController.debitXP);
 
 module.exports = router;

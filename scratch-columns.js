6e3565b4b651ccb6b43f16e8d6f89bb989d38d6c
pravin-unlox <pravin.viswanathan@unloxacademy.com>
2026-07-24 19:33:31 +0530

    Profile Fixes

--- scratch-columns.js
+++
@@ -1,5 +0,0 @@
-require('dotenv').config();
-const pool = require('./src/config/database');
-pool.query("SELECT column_name FROM information_schema.columns WHERE table_name = 'posts'")
-  .then(r => { console.log(r.rows.map(x => x.column_name)); process.exit(0); })
-  .catch(e => { console.error(e); process.exit(1); });
